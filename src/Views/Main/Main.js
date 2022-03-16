4e19bd5aab459b1735fd8dde5e0edcb27dacff10
Andy-Mascaro <95321256+Andy-Mascaro@users.noreply.github.com>
2022-03-16 12:37:44 -0700

    got names to display on screen

--- src/Views/Main/Main.js
+++ src/Views/Main/Main.js
@@ -1,11 +1,23 @@
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 import { fetchPokemon } from '../../services/Pokemon';
 
 export default function Main() {
+  const [pokemon, setPokemon] = useState([]);
   useEffect(() => {
-    fetchPokemon();
+    const fetchData = async () => {
+      const data = await fetchPokemon();
+      setPokemon(data);
+    };
+    fetchData();
   }, []);
     
-  return <div>Main</div>;
+  return (
+
+    <div>
+      {pokemon.map((info) =>(
+        <p key={info.id}>{info.pokemon} </p> 
+      ))}
+    </div>
+  );
   
 }
--- src/services/Pokemon.js
+++ src/services/Pokemon.js
@@ -2,5 +2,6 @@
 export async function fetchPokemon() {
   const resp = await fetch(`https://pokedex-alchemy.herokuapp.com/api/pokedex`);
   const data = await resp.json();
-  console.log(resp); 
+  return data.results;
+ 
 }
